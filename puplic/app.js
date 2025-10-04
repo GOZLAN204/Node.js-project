fa1aabe36e2df31c188083a8a163459ed464a3c0
GOZLAN204 <gozlanghadir295@gmail.com>
2025-10-04 20:07:29 +0300

    added elements and API endpoint

---
+++ puplic/app.js
@@ -0,0 +1,14 @@
+ const API = '/api/projects';
+//const API = '/p';
+
+const grid = document.getElementById('grid');
+const form = document.getElementById('projectForm');
+const formTitle = document.getElementById('formTitle');
+const cancelEditBtn = document.getElementById('cancelEdit');
+const formErr = document.getElementById('formErr');
+
+const idField = document.getElementById('projectId');
+const nameField = document.getElementById('name');
+const descField = document.getElementById('description');
+const imageField = document.getElementById('image');
+const ratingField = document.getElementById('rating');
