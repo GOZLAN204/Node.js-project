ca3b2de8c3c3ca30c4b68c757077a74cdeb10be3
GOZLAN204 <gozlanghadir295@gmail.com>
2025-10-04 19:32:42 +0300

    Initial setup: created Node.js project and basic Express server

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3435;
+
+// Middlewares
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+ app.use('/api/projects', require('./routes/projects'));
+
+// Root 
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(port, () => {
+	console.log(`http://localhost:${port}`);
+});
